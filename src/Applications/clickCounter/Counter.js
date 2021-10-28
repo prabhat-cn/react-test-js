c50f94678cca6f74b4111854db3feb586ea25fb8
gitcoder-23 <prabhatece21@gmail.com>
2021-10-28 21:10:21 +0530

    All

--- src/Applications/clickCounter/Counter.js
+++ src/Applications/clickCounter/Counter.js
@@ -2,9 +2,11 @@ import React, { useState } from 'react';
 
 export const Counter = () => {
   const [count, setCount] = useState(0);
+  const [error, setError] = useState(false);
 
   const decrement = () => {
     if (count <= 0) {
+      setError(true);
       setCount(0);
     } else {
       setCount(count - 1);
@@ -16,6 +18,13 @@ export const Counter = () => {
       <h1 data-test="counter-display">
         Counted Value &nbsp; <span data-test="count">{count}</span>
       </h1>
+      <p>
+        {error && (
+          <span style={{ color: 'yellow' }}>
+            The value can not decrement than 0
+          </span>
+        )}
+      </p>
       <button data-test="increment-button" onClick={() => setCount(count + 1)}>
         Increment Counter
       </button>
