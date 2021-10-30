bfdb6c2b7c4ebc79097642fc241b5dafd6fa725f
gitcoder-23 <prabhatece21@gmail.com>
2021-10-30 22:08:43 +0530

    ✅

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import { Counter } from './Applications/clickCounter/Counter';
+import Congrats from './Applications/jotoApp/congrats/Congrats';
 import GuessedWords from './Applications/jotoApp/guessedWords/GuessedWords';
 
 function App() {
@@ -8,7 +9,13 @@ function App() {
       <h1 className="app-head">React Testing!!</h1>
       <h2 className="app-child">Running Apps Below</h2>
       <Counter />
-      {/* <GuessedWords /> */}
+      <br />
+      <h1>Jotto Guess Game</h1>
+      {/* pass props value depends on test */}
+      <Congrats success={false} />
+      <GuessedWords
+        guessedWords={[{ guessedWord: 'train', letterMatchCount: 3 }]}
+      />
     </div>
   );
 }
