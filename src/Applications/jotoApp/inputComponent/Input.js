422186b1ba2af15665f137e2808866c277fc4d07
gitcoder-23 <prabhatece21@gmail.com>
2021-10-31 09:26:05 +0530

    Primary test input success

--- src/Applications/jotoApp/inputComponent/Input.js
+++ src/Applications/jotoApp/inputComponent/Input.js
@@ -1,11 +1,16 @@
 import React from 'react';
 
-const Input = (props) => {
+import PropTypes from 'prop-types';
+
+const Input = ({ secretWord }) => {
   return (
     <div data-test="component-input">
       <h1>Input</h1>
     </div>
   );
+};
+Input.propTypes = {
+  secretWord: PropTypes.string.isRequired,
 };
 
 export default Input;
--- src/Applications/jotoApp/inputComponent/Input.test.js
+++ src/Applications/jotoApp/inputComponent/Input.test.js
@@ -4,12 +4,17 @@ import Input from './Input';
 import { checkProps, findByTestAttr } from '../../../test/testUtils';
 import '../../../setupTests';
 
-const setup = () => {
-  return shallow(<Input />);
+const setup = (secretWord = 'party') => {
+  return shallow(<Input secretWord={secretWord} />);
 };
 
 test('Input renders without error', () => {
   const wrapper = setup();
   const inputComponent = findByTestAttr(wrapper, 'component-input');
   expect(inputComponent.length).toBe(1);
+});
+
+test('does not throw warning with expected props', () => {
+  // checkProps(component name, { props });
+  checkProps(Input, { secretWord: 'party' });
 });
