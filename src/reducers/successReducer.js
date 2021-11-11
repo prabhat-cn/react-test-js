002d2480a3a1bdf67142dffbfade9a062818a9b1
prabhat-cn <prabhat@capitalnumbers.com>
2021-11-11 12:07:23 +0530

    ne commit

---
+++ src/reducers/successReducer.js
@@ -0,0 +1,10 @@
+/**
+ * @function successReducer
+ * @param {boolean} state - previous state.
+ * @param {object} action - action to be reduced.
+ * @returns {boolean} - new success state.
+ */
+
+export default (state, action) => {
+  return null;
+};
